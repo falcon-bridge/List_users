de3dbe43ab2364e8ee6e306db19f3f7d763c7c7c
Aayush <falcon.bridge2000@gmail.com>
2021-12-02 02:52:56 +0530

    added the user list component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
-import AddUser from "./components/Users/AddUser";
+import AddUser from "./components/users/AddUser";
+import UserList from "./components/users/UserList";
 
 function App() {
+  // const dummy = [{ name: "Aayush", age: 21 }];
+  // const [users, setUsers] = useState([]);
+
   return (
     <div>
       <AddUser />
+      <UserList users={[]} />
     </div>
   );
 }
